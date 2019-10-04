5f4dc788e0fdb3152ff8de2c0532f8a59c84bec4
Tonguc Catakli <Tonguc.Catakli@maximintegrated.com>
2019-10-04 17:24:04 +0300

    Update HspStreamData structure
    Change-Id: Ief8740aaf3873b28163c7a1b2536938303a739df

--- hsp/src/main/java/com/maximintegrated/bpt/hsp/HspStreamData.kt
+++ hsp/src/main/java/com/maximintegrated/bpt/hsp/HspStreamData.kt
@@ -3,12 +3,30 @@ package com.maximintegrated.bpt.hsp
 
 data class HspStreamData(
     val sampleCount: Int,
+    val sampleTime: Float,
+    val green: Int,
+    val green2: Int,
     val ir: Int,
     val red: Int,
-    val green: Int,
     val accelerationX: Float,
     val accelerationY: Float,
     val accelerationZ: Float,
+    val operationMode: Int,
+    val hr: Int,
+    val hrConfidence: Int,
+    val rr: Float,
+    val rrConfidence: Int,
+    val activity: Int,
+    val r: Float,
+    val wspo2Confidence: Int,
+    val spo2: Float,
+    val wspo2PercentageComplete: Int,
+    val wspo2LowSnr: Int,
+    val wspo2Motion: Int,
+    val wspo2LowPi: Int,
+    val wspo2UnreliableR: Int,
+    val wspo2State: Int,
+    val scdState: Int,
     val currentTimeMillis: Long = System.currentTimeMillis()
 ) {
     companion object {
@@ -17,12 +35,30 @@ data class HspStreamData(
         fun fromPacket(packet: ByteArray) = with(BitStreamReader(packet, 8)) {
             HspStreamData(
                 sampleCount = nextInt(8),
+                sampleTime = nextFloat(32, 1),
                 green = nextInt(20),
+                green2 = nextInt(20),
                 ir = nextInt(20),
                 red = nextInt(20),
                 accelerationX = nextSignedFloat(14, 1000),
                 accelerationY = nextSignedFloat(14, 1000),
-                accelerationZ = nextSignedFloat(14, 1000)
+                accelerationZ = nextSignedFloat(14, 1000),
+                operationMode = nextInt(4),
+                hr = nextInt(12),
+                hrConfidence = nextInt(8),
+                rr = nextSignedFloat(14, 10),
+                rrConfidence = nextInt(8),
+                activity = nextInt(4),
+                r = nextSignedFloat(12, 10),
+                wspo2Confidence = nextInt(8),
+                spo2 = nextFloat(11, 10),
+                wspo2PercentageComplete = nextInt(8),
+                wspo2LowSnr = nextInt(1),
+                wspo2Motion = nextInt(1),
+                wspo2LowPi = nextInt(1),
+                wspo2UnreliableR = nextInt(1),
+                wspo2State = nextInt(4),
+                scdState = nextInt(4)
             )
         }
     }
